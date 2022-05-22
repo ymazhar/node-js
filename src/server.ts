b941a77b666f4a9b1a394cc20793dfa48e6fba33
Yaroslav Mazhar <yaroslav.mazhar@pearson.com>
2022-05-22 14:31:05 +0300

    add eslint config

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const PORT: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
