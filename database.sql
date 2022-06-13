be4dba44bf3d1cf484d5d0eed463177bcbc83f8f
Yaroslav Mazhar <yaroslav.mazhar@pearson.com>
2022-06-13 19:22:17 +0300

    delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    login varchar(255) not null,
-    password varchar(255) not null,
-    age int,
-    is_deleted boolean
-)
